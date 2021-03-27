3c6031e94aae0e62b828413278f9b4f150aea5d5
scottydoesntknow4tran <scottytornquist@hotmail.com>
2021-03-27 23:13:59 +0000

    beta-working

--- firstapp/drive.js
+++ firstapp/drive.js
@@ -5,7 +5,7 @@
 
 const Gpio = require('onoff').Gpio;
 //var RELAY = 17;
-const led = new Gpio(17, 'out');
+const led = new Gpio(4, 'out');
 
 //wpi.setup('wpi');// intialization
 
@@ -20,11 +20,11 @@ function pulse_relay(){
  	// //std::this_thread::sleep_for(std::chrono::milliseconds(1000)); // wait to avoid button bouncing
  	// digitalWrite(RELAY, LOW);//powering off relay
  	// pinMode(RELAY, INPUT); //set to input pin to  protect pin
-    led.writeSync(1);
-    setTimeout(() => {led.writeSync(0);}, 200);
+    led.write(1);
+    setTimeout(() => {led.write(0);}, 10000);
     //const iv = setInterval(_ => led.writeSync(1);, 200);
     console.log("door toggled");
-    setTimeout(() => {led.unexport();}, 300);
+    setTimeout(() => {led.unexport();}, 11000);
 };
 
 // process.on('SIGINT', _ => {
